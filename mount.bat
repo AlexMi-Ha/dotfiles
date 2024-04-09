823b3da6732cd9d9eb156cfb1173691769652cd5
Alex <alex@hagl.de>
2024-04-09 09:34:16 +0200

    Windows terminal

--- mount.bat
+++ mount.bat
@@ -4,3 +4,6 @@ mklink .gitconfig %USERPROFILE%\.dotfiles\git\config
 
 cd %USERPROFILE%\AppData\Local
 mklink /D nvim %USERPROFILE%\.dotfiles\nvim
+
+cd %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+mklink settings.json %USERPROFILE%\.dotfiles\windowsterminal\settings.json
