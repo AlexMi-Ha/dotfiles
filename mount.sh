f7b85d0a509716188d515e8d3f3723aad9db1cea
Alex <alex@hagl.de>
2024-06-22 12:53:00 +0200

    Updated mount

--- mount.sh
+++ mount.sh
@@ -1,2 +1,7 @@
+rm -f ~/.gitconfig
 ln -s ~/.dotfiles/git/config ~/.gitconfig
+rm -rf ~/.config/nvim
 ln -s ~/.dotfiles/nvim ~/.config/nvim
+
+rm -f ~/.config/gtk-3.0/bookmarks
+ln -s ~/.dotfiles/linux/.config/gtk-3.0/bookmarks ~/.config/gtk-3.0/bookmarks
